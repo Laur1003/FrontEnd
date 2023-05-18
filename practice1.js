47aa41ad6bdbb7ed47ad94094cdeddd9781f2673
Lauren <Laur10387@msn.com>
2023-05-18 11:03:36 -0400

    css and JS day 1

---
+++ practice1.js
@@ -0,0 +1,36 @@
+// This is how we comment in one line
+/**
+ * This is how we comment in multiple line
+ 
+ */
+
+console.log('Hello World');
+
+console.log("Hello World in double quote");
+
+//3 ways we can define variable
+// var, let
+//const if the variable value is fixed and can not be changed 
+/**
+ * 
+ * Javascript is not a strongly typed language
+ * it is a case sensitive language
+ */
+
+let message = "Hello to javascript" ;
+console.log(message);
+
+var num = 10;
+console.log(num);
+
+let price = 1399.99;
+console.log("Iphone price is " +price);
+
+let hired = true;
+console.log(hired);
+
+let coffee ;
+console.log(coffee);
+
+let negativity = null;
+console.log(negativity);
